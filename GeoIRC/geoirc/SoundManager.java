8ffdb6e2289beb6fe8d46ff2d41d5806afabe82b
pistos <pistos>
2003-07-07 16:14:25 +0000

    no message

---
+++ GeoIRC/geoirc/SoundManager.java
@@ -0,0 +1,93 @@
+/*
+ * SoundManager.java
+ *
+ * Created on July 7, 2003, 8:30 AM
+ */
+
+package geoirc;
+
+import java.util.Vector;
+
+/**
+ *
+ * @author  Pistos
+ */
+public class SoundManager implements GeoIRCConstants
+{
+    protected Vector triggers;
+    protected SettingsManager settings_manager;
+    protected DisplayManager display_manager;
+    
+    // No default constructor
+    private SoundManager() { }
+    
+    /** Creates a new instance of SoundManager */
+    public SoundManager(
+        SettingsManager settings_manager,
+        DisplayManager display_manager
+    )
+    {
+        this.settings_manager = settings_manager;
+        this.display_manager = display_manager;
+        triggers = new Vector();
+        
+        int i = 0;
+        String i_str;
+        String filter;
+        String regexp;
+        String filename;
+        while( GOD_IS_GOOD )
+        {
+            i_str = Integer.toString( i );
+            
+            filter = settings_manager.getString(
+                "/sound/triggers/" + i_str + "/filter",
+                ""
+            );
+            if( filter.equals( "" ) )
+            {
+                // No more triggers stored in the settings.
+                break;
+            }
+            
+            regexp = settings_manager.getString(
+                "/sound/triggers/" + i_str + "/regexp",
+                ""
+            );
+            filename = settings_manager.getString(
+                "/sound/triggers/" + i_str + "/filename",
+                ""
+            );
+            
+            addTrigger( filter, regexp, filename );
+            
+            i++;
+        }
+    }
+    
+    public void check( String message, String qualities )
+    {
+        int n = triggers.size();
+        SoundTrigger st;
+        for( int i = 0; i < n; i++ )
+        {
+            st = (SoundTrigger) triggers.elementAt( i );
+            st.check( message, qualities );
+        }
+    }
+    
+    protected boolean addTrigger( String filter, String regexp, String sound_file )
+    {
+        boolean success = true;
+        SoundTrigger st;
+        try
+        {
+            st = new SoundTrigger( display_manager, filter, regexp, sound_file );
+        }
+        catch( java.util.regex.PatternSyntaxException e )
+        {
+            success = false;
+        }
+        return success;
+    }
+}
---
+++ GeoIRC/geoirc/SoundTrigger.java
@@ -0,0 +1,87 @@
+/*
+ * SoundTrigger.java
+ *
+ * Created on July 7, 2003, 8:04 AM
+ */
+
+package geoirc;
+
+import com.antelmann.sound.*;
+import java.io.*;
+import java.util.regex.*;
+
+/**
+ *
+ * @author  Pistos
+ */
+public class SoundTrigger
+{
+    String filter;
+    SoundPlayer player;
+    DisplayManager display_manager;
+    Pattern regexp;
+    
+    // No default constructor.
+    private SoundTrigger() { }
+    
+    public SoundTrigger(
+        DisplayManager display_manager,
+        String filter,
+        String regexp_str,
+        String sound_file
+    )
+    throws PatternSyntaxException
+    {
+        this.display_manager = display_manager;
+        this.filter = filter;
+
+        try
+        {
+            regexp = Pattern.compile( regexp_str );
+        }
+        catch( PatternSyntaxException e )
+        {
+            display_manager.printlnDebug(
+                "Regular expression syntax error for expression '"
+                + regexp_str + "'"
+            );
+            display_manager.printlnDebug( e.getMessage() );
+            throw e;
+        }
+        
+        try
+        {
+            player = new SoundPlayer( new File( sound_file ) );
+        }
+        catch( SoundException e )
+        {
+            display_manager.printlnDebug( e.getMessage() );
+        }
+    }
+    
+    /* Check against a message which has certain qualities.
+     * Play the sound if the check passes.
+     */
+    public boolean check( String message, String qualities )
+    {
+        boolean passed = false;
+        try
+        {
+            passed =
+                BoolExpEvaluator.evaluate( filter, qualities )
+                && regexp.matcher( message ).matches();
+        }
+        catch( BadExpressionException e )
+        {
+            display_manager.printlnDebug( "Filter evaluation error for filter '" + filter + "'" );
+            display_manager.printlnDebug( e.getMessage() );
+        }
+        
+        if( passed )
+        {
+            player.play();
+        }
+        
+        return passed;
+    }
+}
