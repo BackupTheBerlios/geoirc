2f48d4608b8bb0d130aa251b0f5fec6cbdc6efcc
pistos <pistos>
2003-07-08 12:41:51 +0000

    no message

---
+++ GeoIRC/geoirc/StyleManager.java
@@ -0,0 +1,19 @@
+/*
+ * StyleManager.java
+ *
+ * Created on July 8, 2003, 8:40 AM
+ */
+
+package geoirc;
+
+/**
+ *
+ * @author  Pistos
+ */
+public class StyleManager
+{
+    
+    // No default constructor.
+    private StyleManager() { }
+    
+}
