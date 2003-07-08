bcbed543cf271348ba666ccdd519aab5b00f97af
pistos <pistos>
2003-07-08 13:58:00 +0000

    no message

--- GeoIRC/geoirc/StyleManager.java
+++ GeoIRC/geoirc/StyleManager.java
@@ -6,6 +6,7 @@
 
 package geoirc;
 
+import javax.swing.*;
 import javax.swing.text.*;
 
 /**
@@ -37,22 +38,6 @@ public class StyleManager
             base_style,
             settings_manager.getInt( "/gui/text windows/font size", 14 )
         );
-    }
-
-    public void initializeTextPane( JTextPane pane )
-    {
-        /*
-        Style style_normal = text_pane.addStyle( "normal", base_style );
-        
-        Style s = text_pane.addStyle( "italic", style_normal );
-        StyleConstants.setItalic( s, true );
-        
-        s = text_pane.addStyle( "bold", style_normal );
-        StyleConstants.setBold( s, true );
-        
-        s = text_pane.addStyle( "underline", style_normal );
-        StyleConstants.setUnderline( s, true );
-         */
         
         // Read in more styles based on highlight settings.
         
@@ -60,6 +45,11 @@ public class StyleManager
         s = text_pane.addStyle( "blue", style_normal );
         StyleConstants.setBackground( s, new Color( 0, 0, 255 ) );
          */
+    }
+
+    public void initializeTextPane( JTextPane text_pane )
+    {
+        text_pane.addStyle( "normal", base_style );
         
     }
     
