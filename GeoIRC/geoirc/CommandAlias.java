67e5beaacd2cf411db44b2fcd9de86bf49f97348
pistos <pistos>
2003-07-09 22:43:24 +0000

    no message

--- GeoIRC/geoirc/CommandAlias.java
+++ GeoIRC/geoirc/CommandAlias.java
@@ -6,11 +6,13 @@
 
 package geoirc;
 
+import geoirc.util.Util;
+
 /**
  *
  * @author  Pistos
  */
-public class CommandAlias
+public class CommandAlias implements GeoIRCConstants
 {
     protected String alias;
     protected String expansion;
@@ -28,7 +30,35 @@ public class CommandAlias
         return alias;
     }
     
-    public String expand( String input )
+    public String expand( String arg_string )
     {
+        String retval = null;
+        String [] expansion_array = Util.tokensToArray( expansion );
+        String [] args = Util.tokensToArray( arg_string );
+        String token;
+        String token_remainder;
+        
+        for( int i = 0; i < expansion_array.length; i++ )
+        {
+            token = expansion_array[ i ];
+            token_remainder = token.substring( 1 );
+            switch( token.charAt( 0 ) )
+            {
+                case ALIAS_ARG_CHAR:
+                    try
+                    {
+                    }
+                    catch( NumberFormatException e ) { }
+                    break;
+                case ALIAS_ARG_REST_CHAR:
+                    break;
+                default:
+                    break;
+            }
+        }
+        
+        retval = Util.stringArrayToString( expansion_array );
+        
+        return retval;
     }
 }
