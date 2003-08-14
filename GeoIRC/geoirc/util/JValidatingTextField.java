a50de1851b2295db28b31248856748c5f13884ae
netseeker <netseeker>
2003-08-14 13:09:32 +0000

    initial checkin A validating text input field with validating based on regular expressions. As long as the inserted value does not validate a red border will be drawn around the input field.

---
+++ GeoIRC/geoirc/util/JValidatingTextField.java
@@ -0,0 +1,141 @@
+/*
+ * JValidatingTextField.java
+ * 
+ * Created on 14.08.2003
+ */
+package geoirc.util;
+
+import java.awt.Color;
+import java.util.regex.Pattern;
+
+import javax.swing.BorderFactory;
+import javax.swing.JTextField;
+import javax.swing.border.Border;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import javax.swing.text.Document;
+
+/**
+ * A validating text input field with validating based on regular expressions.<br>
+ * As long as the inserted value does not validate a red<br>
+ * border will be drawn around the input field.
+ * @author netseeker aka Michael Manske
+ */
+public class JValidatingTextField
+	extends JTextField
+	implements DocumentListener
+{
+	private static final Border normalBorder =
+		BorderFactory.createLineBorder(Color.BLACK);
+	private static final Border errorBorder =
+		BorderFactory.createLineBorder(Color.RED);
+	private boolean textValid;
+	private Pattern pattern;
+
+	/**
+	 * Creates a new instance
+	 */
+	public JValidatingTextField()
+	{
+		super();
+	}
+	
+	/**
+	 * Creates a new instance
+	 * @param regex regular expression to use for validating input
+	 */
+	public JValidatingTextField(String regex)
+	{
+		super();
+		setPattern(regex);
+	}
+
+	/**
+	 * Creates a new instance with an available value
+	 * @param value value to be inserted in the text field
+	 * @param regex regular expression to use for validating input
+	 */
+	public JValidatingTextField(Object value, String regex)
+	{
+		super();
+		setPattern(regex);
+		this.setText(value.toString());
+	}
+
+	/* (non-Javadoc)
+	 * @see javax.swing.text.JTextComponent#setDocument(javax.swing.text.Document)
+	 */
+	public void setDocument(Document doc)
+	{
+		if (getDocument() != null)
+		{
+			getDocument().removeDocumentListener(this);
+		}
+		super.setDocument(doc);
+		if (getDocument() != null)
+		{
+			getDocument().addDocumentListener(this);
+		}
+		validateText();
+	}
+
+	/**
+	 * sets a new regular expression used for validating
+	 * @param regex regular expression to use for validating input
+	 */
+	public void setPattern(String regex)
+	{
+		pattern = Pattern.compile(regex);
+		validateText();
+	}
+
+	protected void validateText()
+	{
+		String t = getText();
+		boolean valid = t.length() == 0 || pattern.matcher(t).matches();
+		setTextValid(valid);
+	}
+
+	/**
+	 * checks if the inserted value does validate
+	 * @return true if the inserted value validates
+	 * otherwise false
+	 */
+	public boolean isTextValid()
+	{
+		return textValid;
+	}
+
+	protected void setTextValid(boolean valid)
+	{
+		if (textValid ^ valid)
+		{
+			textValid = valid;
+			firePropertyChange("textValid", !valid, valid);
+			setBorder(valid ? normalBorder : errorBorder);
+		}
+	}
+	/* (non-Javadoc)
+	 * @see javax.swing.event.DocumentListener#insertUpdate(javax.swing.event.DocumentEvent)
+	 */
+	public void insertUpdate(DocumentEvent arg0)
+	{
+		validateText();	
+	}
+	
+	/* (non-Javadoc)
+	 * @see javax.swing.event.DocumentListener#removeUpdate(javax.swing.event.DocumentEvent)
+	 */
+	public void removeUpdate(DocumentEvent arg0)
+	{
+		validateText();		
+	}
+	
+	/* (non-Javadoc)
+	 * @see javax.swing.event.DocumentListener#changedUpdate(javax.swing.event.DocumentEvent)
+	 */
+	public void changedUpdate(DocumentEvent arg0)
+	{
+		validateText();		
+	}
+}
