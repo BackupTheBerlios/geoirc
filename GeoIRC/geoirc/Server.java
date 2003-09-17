05b14f7e4e98c495efbdcba06bf9c6d0b8b6b7ed
netseeker <netseeker>
2003-09-17 23:09:47 +0000

    added optional display of dns username and host in join messages

--- GeoIRC/geoirc/Server.java
+++ GeoIRC/geoirc/Server.java
@@ -6,20 +6,24 @@
 
 package geoirc;
 
-import geoirc.util.*;
-import java.awt.Component;
-import java.io.*;
+import geoirc.util.BadExpressionException;
+import geoirc.util.BoolExpEvaluator;
+import geoirc.util.Util;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.text.DateFormat;
 import java.util.Date;
 import java.util.HashSet;
-import java.util.Hashtable;
 import java.util.Iterator;
 import java.util.Set;
 import java.util.Vector;
-import java.util.regex.*;
-import javax.swing.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  *
