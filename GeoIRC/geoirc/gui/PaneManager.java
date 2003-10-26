bdf01569bd2b973d83d5478c5c517ca04144c50c
pistos <pistos>
2003-10-26 20:32:07 +0000

    no message

---
+++ GeoIRC/geoirc/gui/PaneManager.java
@@ -0,0 +1,17 @@
+/*
+ * PaneManager.java
+ *
+ * Created on October 26, 2003, 12:35 PM
+ */
+
+package geoirc.gui;
+
+/**
+ *
+ * @author  Pistos
+ */
+public class PaneManager extends java.util.Vector
+{
+    
+    
+}
